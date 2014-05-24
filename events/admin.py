753eabe850a9e2d7a92f6d5f7e1169336c44c694
mhg <michigraber@gmail.com>
2014-05-24 23:34:36 +0200

    events model first take

--- events/admin.py
+++ events/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
 
-# Register your models here.
+from . import models
+
+
+class EventAdmin(admin.ModelAdmin):
+    
+    list_display = ('title_en', 'date_from', 'date_until', )
+    prepopulated_fields = {"slug": ("title_en",)}
+
+
+class EventPartAdmin(admin.ModelAdmin):
+    pass
+
+class RegistrationAdmin(admin.ModelAdmin):
+    pass
+
+class ArticleAdmin(admin.ModelAdmin):
+    pass
+    
+class PurchaseAdmin(admin.ModelAdmin):
+    pass
+    
+class PurchaseItemAdmin(admin.ModelAdmin):
+    list_display = ('one_line_description', )
+    
+admin.site.register(models.Event, EventAdmin)
+admin.site.register(models.EventPart, EventPartAdmin)
+admin.site.register(models.Registration, RegistrationAdmin)
+admin.site.register(models.Article, ArticleAdmin)
+admin.site.register(models.Purchase, PurchaseAdmin)
+admin.site.register(models.PurchaseItem, PurchaseItemAdmin)
--- events/models.py
+++ events/models.py
@@ -1,21 +1,34 @@
 from django.db import models
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes import generic
+
+from associates.models import Associate
 
 
 
-class Event(models.model):
+class Event(models.Model):
     '''
     '''
-    title = models.CharField(max_length=511)
+    title_de = models.CharField(max_length=511)
+    title_en = models.CharField(max_length=511)
+
     date_from = models.DateField()
     date_until = models.DateField()
-    participant_address = models.TextField()
-    description = models.TextField()
-    schedule = models.TextField()
+
+    slug = models.SlugField()
+
+    description_de = models.TextField(blank=True, null=True)
+    description_en = models.TextField(blank=True, null=True)
+
+    schedule_de = models.TextField(blank=True, null=True)
+    schedule_en = models.TextField(blank=True, null=True)
 
     show_event = models.BooleanField(default=True)
 
+    event_part_set_price_map_json = models.CharField(max_length=255)
 
-class EventPart(models.model):
+
+class EventPart(models.Model):
     '''
     '''
     event = models.ForeignKey(Event)
@@ -23,48 +36,103 @@ class EventPart(models.model):
     datetime_from = models.DateTimeField()
     datetime_until = models.DateTimeField()
 
-    short_description = models.CharField(max_length=511)
+    short_description_de = models.CharField(max_length=511)
+    short_description_en = models.CharField(max_length=511)
 
     is_bookable = models.BooleanField()
-    price_sfr = models.PositiveSmallIntegerField()
     max_number_of_participants = models.PositiveSmallIntegerField()
 
 
-class Registration(models.models):
+class Registration(models.Model):
     '''
     '''
-    event_parts = models.ManyToManyRelationshipField(EventPart)
-    total_price = model.PositiveSmallIntegerField()
+    associate = models.ForeignKey(Associate)
+    event_parts = models.ManyToManyField(EventPart)
+    price = models.PositiveSmallIntegerField(blank=True, null=True)
+    PAID_BY_CHOICES = (
+            (0, 'not paid'),
+            (1, 'cash'),
+            (2, 'paypal'),
+            (4, 'bank transfer'),
+            )
+    paid_by = models.SmallIntegerField(choices=PAID_BY_CHOICES)
 
-    
-class Item(models.model):
+    def one_line_description(self, language='de'):
+        if language == 'de':
+            return 'Registration ({pk}) : {fn} {ln}'.format(
+                    pk=self.pk,
+                    fn=self.associate.first_name, ln=self.associate.last_name)
+        elif language == 'en':
+            return 'Registration ({pk}) : {fn} {ln}'.format(
+                    pk=self.pk,
+                    fn=self.associate.first_name, ln=self.associate.last_name)
+        else:
+            return 'Registration ({pk}) : {fn} {ln}'.format(
+                    pk=self.pk,
+                    fn=self.associate.first_name, ln=self.associate.last_name)
+
+ 
+class Article(models.Model):
     '''
     '''
-    name = models.CharField(max_length=255)
-    description = models.TextField()
+    name_de = models.CharField(max_length=255)
+    name_en = models.CharField(max_length=255)
 
-    is_sold = models.BooleanField()
-    price_sfr = models.PositiveSmallIntegerField()
-    number_of_items = models.PositiveSmallIntegerField()
+    description_de = models.TextField(null=True, blank=True)
+    description_en = models.TextField(null=True, blank=True)
+
+    article_image = models.ImageField(upload_to='articles', blank=True,
+            null=True)
+
+    events = models.ManyToManyField(Event, blank=True, null=True)
+
+    is_sold = models.BooleanField(default=True)
+    price = models.PositiveSmallIntegerField()
+    number_of_items_available = models.PositiveSmallIntegerField()
+
+    def one_line_description(self, language='de'):
+        if language == 'de':
+            return self.name_de
+        elif language == 'en':
+            return self.name_en
+        else:
+            return 'Article - no name in language %s' % language
 
 
-class Purchase(models.model):
+class Purchase(models.Model):
     '''
     '''
-    member = models.ForeignKey(Member)
-    # XXX 
-    ITEM_TYPE_CHOICES = models.Q(app_label='member', model='item') |\
-            models.Q(app_label='member', model='registration') 
-    object_type = models.ForeignKey(generic.ContentType, 
-            limit_choices_to=ITEM_TYPE_CHOICES)
-    
-    item = models.GenericRelationField(EventPurchase)
-
+    associate = models.ForeignKey(Associate)
     is_paid = models.BooleanField()
+    comment = models.TextField(blank=True, null=True)
 
-    comment = models.TextField()
+#   def __str__(self):
+#       items = self.purchaseitem_set.all()
+#       if len(items) > 0:
+#           s = '\n'.join([str(i.one_line_description)+'\t'+str(i.price)+'.-'
+#               for i in items])
+#       else:
+#           s = 'Purchase without items!!'
+#       return s
 
 
+class PurchaseItem(models.Model):
+    '''
+    '''
+    ITEM_TYPE_CHOICES = models.Q(app_label='events', model='article') |\
+            models.Q(app_label='events', model='registration') 
+    content_type = models.ForeignKey(generic.ContentType,
+            limit_choices_to=ITEM_TYPE_CHOICES)
+    object_id = models.PositiveIntegerField()
+    content_object = generic.GenericForeignKey('content_type', 'object_id')
+    
+    purchase = models.ForeignKey(Purchase)
 
+    @property
+    def price(self):
+        self.content_object.price
+
+    def one_line_description(self, language='en'):
+        return self.content_object.one_line_description(language=language)
 
 
