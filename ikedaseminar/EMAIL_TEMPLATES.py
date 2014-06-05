056dd02372935a745dfe50ef10e71757de6c1fed
mhg <michigraber@gmail.com>
2014-06-05 17:02:33 +0200

    new EMAIL TEMPLATES added.

---
+++ ikedaseminar/EMAIL_TEMPLATES.py
@@ -0,0 +1,60 @@
+
+REGISTRATION_EMAIL_EN = '''
+
+Hi {first_name},
+
+Thanks for registering for our Seminar with Hiroshi Ikeda Shihan!
+
+Your registration id is : {pid}
+
+Your registration contains 
+
+{package}
+
+We registered you as
+
+{associate}
+
+
+Your message for us:
+
+{message}
+
+
+You will here from us shortly before the seminar.
+
+Kind regards,
+Michael
+'''
+
+
+REGISTRATION_EMAIL_DE = '''
+
+Hi {first_name},
+
+Herzlichen Dank fuer deine Anmeldung zum Seminar mit Hiroshi Ikeda Shihan!
+
+Deine Anmeldungs-id ist : {pid}
+
+Hierfuer hast du dich angemeldet:
+
+{package}
+
+
+So haben wir dich registriert:
+
+{associate}
+
+
+Deine Mitteilung an uns:
+
+{message}
+
+
+Du wirst kurz vor dem Seminar wieder von uns hoeren.
+
+Herzliche Gruesse,
+Michael
+'''
+
+
