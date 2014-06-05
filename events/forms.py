fa40298d63d7bd3bc9f29829ff24cd83f9cf9435
mhg <michigraber@gmail.com>
2014-06-05 19:44:01 +0200

    pretty print forms

--- events/forms.py
+++ events/forms.py
@@ -34,20 +34,18 @@ class SelectPurchaseItemsForm(forms.Form):
                 if language == 'en':
                     self.fields[ep.short_description_en] = BooleanField(
                             required=False)
-                    self.fields[ep.short_description_en].label =\
-                            ep.short_description_en
                 elif language == 'de':
                     self.fields[ep.short_description_de] = BooleanField(
                             required=False)
-                    self.fields[ep.short_description_de].label =\
-                            ep.short_description_de
+                self.fields[ep.short_description_de].label =\
+                        ep.pretty_print(language=language)
 
         for art in event.article_set.all():
             if art.still_available() and art.is_sold:
                 if language == 'en':
                     self.fields[art.name_en] = BooleanField(required=False)
-                    self.fields[art.name_en].label = art.name_en
                 elif language == 'de':
                     self.fields[art.name_de] = BooleanField(required=False)
-                    self.fields[art.name_de].label = art.name_de
+                self.fields[art.name_de].label = art.pretty_print(
+                        language=language)
 
