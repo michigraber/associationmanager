b27809bc14197d3a6a76b60b67730960f61c1468
mhg <michigraber@gmail.com>
2014-07-25 01:19:17 +0200

    last info email templated

--- ikedaseminar/EMAIL_TEMPLATES.py
+++ ikedaseminar/EMAIL_TEMPLATES.py
@@ -66,3 +66,125 @@ Michael
 '''
 
 
+LAST_INFO_EMAIL_DE = '''
+Hi {first_name}
+
+
+Wir freuen uns dich bald zu unserem Aikido Seminar mit Hiroshi Ikeda Shihan
+begruessen zu duerfen. Hier noch einige letzte Infos:
+
+Du hast dich fuer die folgenden Dinge eingeschrieben:
+
+{package}
+
+
+Das Keiko Freitag Abend findet bei uns im Dojo statt:
+
+Aiki-Kai Zuerich 
+Hardturmstrasse 169
+8005 Zuerich
+
+Tramhaltestelle Fischerweg, Tramlinie 17
+
+
+Samstag und Sonntag befinden wir uns in der Sportanlage der Universitaet
+Zuerich, Irchel:
+
+Sportanlagen Universitaet Irchel    
+Winterthurerstrasse 180
+8057 Zuerich
+
+Tramhaltestelle Irchel, Tramlinie 10 und 9
+
+
+Und nun noch:
+
+- Sei bitte rechtzeitig vor Ort damit Sensei das Keiko puenktlich und ungestoert
+  beginnen kann.
+- Die Sportanlage Irchel wird waehrend dem Keiko geschlossen werden und du
+  wirst klingeln muessen um Einlass zu erhalten.
+- Du wirst Zoris/Slippers fuer den Weg von der Garderobe bis zu den Tatami
+  brauchen.
+- Bring bitte Jo und Bokken mit damit Sensei diese mit uns verwenden kann wenn
+  er moechte.
+
+Wir legen Samstag Morgen gemeinsam die Matten aus und sind froh wenn du uns
+helfen kommen kannst und dich dafuer bereits ca 09:00 Uhr in der Sportanlage
+Irchel einfindest.
+
+
+Die Party Samstag Abend findet im GZ Wipkingen statt:
+
+GZ Wipkingen
+Breitensteinstrasse 19 a
+8037 Zuerich
+
+Tramhaltestelle Foerrlibuckstrasse, Tramlinie  17 oder
+Tramhaltestelle Wipkingerplatz, Tramlinie 13
+
+
+Herzliche Gruesse und bis bald,
+Michael
+'''
+
+
+
+LAST_INFO_EMAIL_EN = '''
+Hi {first_name},
+
+
+We are looking forward to seeing you soon on our aikido seminar with Hiroshi
+Ikeda Shihan. We send you here the latest Information.
+
+You registered for the following things:
+
+{package}
+
+
+The keiko on Friday evening will take place in our dojo:
+
+Aiki-Kai Zuerich 
+Hardturmstrasse 169
+8057 Zuerich
+
+Tram stop Fischerweg, tram line 14
+
+
+On Saturday and Sunday the keiko will take place in the Sport Center of the
+University of Zuerich Irchel.
+
+Sportanlagen Universitaet Irchel    
+Winterthurerstrasse 190
+8057 Zuerich
+
+Tram stop Irchel, tram line 10 und 9
+
+And finally:
+
+- Please be on time on the mats so that Sensei can begin with the Keiko
+  timely and uninterruptedly.
+- We will have to close the doors to the Sports Center at the University during
+  the keiko. Therefore you will have to ring the bell to be let in should you
+  run late.
+- You will need zoris/slippers for the way from the wardrobe to the tatami.
+- Please bring Jo and Bokken so that Sensei could use them together with us in
+  case he wanted to.
+
+We will lay out the mats on Saturday morning in a collective effort and are
+glad for all helping hands. Therefore we kindly ask you to join us already
+around 09:00 at the Irchel Sports Center.
+
+
+The Party Saturday evening will take place in GZ Wipkingen:
+
+GZ Wipkingen
+Breitensteinstrasse 19 a
+8037 Zuerich
+
+Tram stop Foerrlibuckstrasse, tram line  17 or
+Tram stop Wipkingerplatz, tram line 13
+
+
+Kind regards and see you soon!
+Michael
+'''
