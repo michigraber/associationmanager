80669522297bcae222ac0f455193b0d0d034e2ee
mhg <michigraber@gmail.com>
2015-04-12 14:06:51 +0900

    memory view added.

--- ikedaseminar/views.py
+++ ikedaseminar/views.py
@@ -1,6 +1,40 @@
 import random
 from django.shortcuts import render_to_response
 
+def memory(request, language=''):
+
+    ROW_LENGTH = 5 
+
+    friday_no_imgs = 70
+    rows = []
+    row = []
+    
+    for i in range(1, friday_no_imgs+1):
+        if len(row) % ROW_LENGTH == 0:
+            row = []
+        if i < 10:
+            strnum = '0'+str(i)
+        else:
+            strnum = str(i)
+        row.append(strnum)
+        if len(row) == ROW_LENGTH:
+            rows.append(row)
+
+    albums = [
+            {
+                'name_de' : 'Ikeda Seminar Z&uuml;rich 2014 - Freitag',
+                'name_en' : 'Ikeda Seminar Z&uuml;rich 2014 - Friday',
+                'prefix' : 'IkedaZurich2014Friday', 
+                'rows': rows,
+                }
+            ]
+
+    ddict = { 
+            'language': language,
+            'albums': albums,
+            }
+    return render_to_response('memory.html', ddict )
+
 
 def welcome(request, language=''):
     ddict = { 
