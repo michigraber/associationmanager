aaa2652603d28b883320a5d146b6569ec15a0b0e
Michael Graber <michael.graber@gmail.com>
2016-07-02 19:37:58 +0200

    registration offline before

--- ikedaseminar/views.py
+++ ikedaseminar/views.py
@@ -47,6 +47,6 @@ def welcome(request, language=''):
 def registration(request, language='de'):
     ddict = { 
             'language': language,
-            'reason': 'end',
+            'reason': 'before',
             }
     return render_to_response('registration_offline.html', ddict )
