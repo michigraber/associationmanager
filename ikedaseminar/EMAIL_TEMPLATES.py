2c103a1c36d8af985f6093165c52bccdfd94278a
Michael Graber <michael.graber@fhnw.ch>
2017-08-30 13:08:17 +0200

    registration email updated

--- ikedaseminar/EMAIL_TEMPLATES.py
+++ ikedaseminar/EMAIL_TEMPLATES.py
@@ -8,7 +8,7 @@ Thanks for registering for our Seminar with Hiroshi Ikeda Shihan!
 Your registration id is : {pid}
 
 
-The seminar takes place from Friday, September 30 until Sunday, October 2, 2016.
+The seminar takes place from Friday, September 29 until Sunday, October 1, 2017.
 
 
 Your registration contains 
@@ -41,7 +41,7 @@ Herzlichen Dank fuer deine Anmeldung zum Seminar mit Hiroshi Ikeda Shihan!
 Deine Anmeldungs-id ist : {pid}
 
 
-Das Seminar findet von Freitag 30. Septeber bis Sonntag 2. Oktober 2016 statt.
+Das Seminar findet von Freitag 29. September bis Sonntag 1. Oktober 2017 statt.
 
 
 Hierfuer hast du dich angemeldet:
